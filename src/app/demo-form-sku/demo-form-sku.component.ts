dd0d61bad833f5eb1f980daf63177be34b18ad79
Abir Mahmud <amabirmahmud@gmail.com>
2021-05-02 14:23:50 +0600

    created demo-form-sku component

---
+++ src/app/demo-form-sku/demo-form-sku.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-demo-form-sku',
+  templateUrl: './demo-form-sku.component.html',
+  styleUrls: []
+})
+export class DemoFormSkuComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
