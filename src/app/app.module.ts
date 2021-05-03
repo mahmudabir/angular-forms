52058cea87c3d2c4782f6415520c36ea5b23c7e3
Abir Mahmud <amabirmahmud@gmail.com>
2021-05-03 23:56:06 +0600

    Custom Validation
    created demo-form-with-custom-validation component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { DemoFormSkuComponent } from './demo-form-sku/demo-form-sku.component';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {RouterModule} from '@angular/router';
 import { DemoFormWithValidationsExplicitComponent } from './demo-form-with-validations-explicit/demo-form-with-validations-explicit.component';
+import { DemoFormWithCustomValidationComponent } from './demo-form-with-custom-validation/demo-form-with-custom-validation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DemoFormSkuComponent,
-    DemoFormWithValidationsExplicitComponent
+    DemoFormWithValidationsExplicitComponent,
+    DemoFormWithCustomValidationComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/demo-form-with-custom-validation/demo-form-with-custom-validation.component.ts
@@ -0,0 +1,46 @@
+import {Component, OnInit} from '@angular/core';
+import {AbstractControl, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+
+/**
+ * Our custom validator
+ *
+ * A validator:
+ * - Takes a `Control` as it's input and
+ * - Returns a `StringMap<string, boolean>` where the key is "error code" and
+ *   the value is `true` if it fails
+ */
+function skuValidator(control: FormControl): { [s: string]: boolean } {
+  if (!control.value.match(/^123/)) {
+    return {invalidSku: true};
+  }
+  return {invalidSku: false};
+}
+
+
+@Component({
+  selector: 'app-demo-form-with-custom-validation',
+  templateUrl: './demo-form-with-custom-validation.component.html',
+  styleUrls: []
+})
+export class DemoFormWithCustomValidationComponent implements OnInit {
+  myForm: FormGroup;
+  sku: AbstractControl;
+
+  constructor(fb: FormBuilder) {
+    this.myForm = fb.group({
+      sku:  ['', Validators.compose([
+        Validators.required, skuValidator])]
+    });
+
+    this.sku = this.myForm.controls.sku;
+  }
+
+
+  onSubmit(value: string): void {
+    console.log('you submitted value: ', value);
+  }
+
+  ngOnInit(): void {
+  }
+
+}
